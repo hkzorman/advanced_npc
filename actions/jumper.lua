5a6113479ec0470eab83788c71874e1d6fa76fdc
Hector Kio <hector.kio223@gmail.com>
2017-04-05 19:18:34 -0400

    Improve initial comment.

--- actions/jumper.lua
+++ actions/jumper.lua
@@ -2,8 +2,9 @@
 -- This code is entirely based on the Jumper library by Roland Yonaba.
 -- The modifications are only to make it work under Minetest's secure
 -- environment. Therefore, the code in this file is under the MIT license
--- as the original Jumper library. The original library code can be found
--- here: https://github.com/Yonaba/Jumper
+-- as the original Jumper library (please see copyright notice below). 
+-- The original library code can be found here: 
+-- https://github.com/Yonaba/Jumper
 
 -- Modifications are by Hector Franqui (Zorman2000)
 
