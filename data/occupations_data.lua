cb93e133788791362fcc4a9f2962a15a395b7169
NewbProgrammer101 <countolaf17@gmail.com>
2017-09-08 06:12:16 -0500

    Delete occupations_data.lua

--- data/occupations_data.lua
+++
@@ -1,7 +0,0 @@
-
-local path = minetest.get_modpath("advanced_npc")
-
--- This may be temporary for now.
-dofile(path.."/data/occupations/default.lua")
-dofile(path.."/data/occupations/default_farmer.lua")
-dofile(path.."/data/occupations/default_priest.lua")
